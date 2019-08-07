3cdfb7fc96293441104354343e94c80aef855a5f
sulianto.sulaiman <sulianto.sulaiman@TRAINING016.optimum.net>
2019-08-07 16:25:44 +0800

    Created login method

---
+++ src/application/PayNowApplication.java
@@ -0,0 +1,15 @@
+package application;
+
+public class PayNowApplication 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    void login()
+    {
+	   	 System.out.println("This is a login method");
+	   	 System.out.println("Version 1");
+    }
+}
