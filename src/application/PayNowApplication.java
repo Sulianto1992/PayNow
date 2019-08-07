aa5d8fd8ff3d3d21297f2065206150d1740f37ec
jiarong.ng <jiarong.ng@OPT-HO-8XY5HD2.optimum.net>
2019-08-07 16:50:08 +0800

    Feature - withdraw method created

--- src/application/PayNowApplication.java
+++ src/application/PayNowApplication.java
@@ -11,5 +11,13 @@ public class PayNowApplication
     {
 	   	 System.out.println("This is a login method");
 	   	 System.out.println("Version 1");
+	   	 System.out.println("This is a develop branch");
+    }
+    
+    void withdrawMoney() {
+    	System.out.println("This is a withdraw method");
+	   	 System.out.println("Version 2");
+	   	 System.out.println("This is a develop branch");
+	   	 System.out.println("This is written by Developer B");
     }
 }
